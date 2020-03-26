fb727654a3f7c29d28a237672ea25c1af7483011
Dina Kendle <dina.kendle@gmail.com>
2020-03-26 18:52:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ def speak_to_grandma(string)
       "HUH?! SPEAK UP, SONNY!"
       
     elsif string == string.capitalize
-      ""
+      "HUH?! SPEAK UP, SONNY!"
       
     elsif string == string.upcase
       "NO, NOT SINCE 1938!"
