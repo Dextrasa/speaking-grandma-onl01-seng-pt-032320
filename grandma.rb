ef6ab7a57601160426b712222883f5713ea19d3e
Dina Kendle <dina.kendle@gmail.com>
2020-03-26 18:54:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@ def speak_to_grandma(string)
     elsif string == string.swapcase
       "HUH?! SPEAK UP, SONNY!"
       
-    elsif string == string.upcase
+    else string == string.upcase
       "NO, NOT SINCE 1938!"
     
     end
