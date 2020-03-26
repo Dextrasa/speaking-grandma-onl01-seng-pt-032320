0baebd0adc1383813619444f6220ea667bb9ee67
Dina Kendle <dina.kendle@gmail.com>
2020-03-26 18:42:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 
-def speak_to_grandma(phrase)
+def speak_to_grandma(string)
   
-    if #{phrase} == "I LOVE YOU GRANDMA!"
+    if #{string} == "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
       
 end
